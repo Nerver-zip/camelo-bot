26d5fb375659ffd11cb5070f54a8c5fc47e6b654
Nerver <marceloaugusto12161@gmail.com>
2025-05-18 15:35:03 -0300

    Updated help

--- src/commands/cameloHelp.js
+++ src/commands/cameloHelp.js
@@ -9,10 +9,13 @@ module.exports = {
     `Administrativos ⚙️\n\n` +
     `!createChannels <categoria> mermail hero blue-eyes...: cria os canais indicados sob a categoria\n\n` +
     `!moveChannels <categoria> mermail hero blue-eyes: move canais para a categoria referenciada\n\n` +
-    `!organizeChannels <categoria>: ordena em ordem alfabética crescente os canais da categoria\n\n` +
+    `!organizeChannels <categoria>: ordena em ordem alfabética crescente os canais da categoria\n\n`;
+    }
+    
+    reply += `Gerais 🌐\n\n ` +
     `!getTierList: atualiza a tier list com base no DLM\n\n` +
     `!skill nome da skill: pega informações da skill`;
-    }   
+    
     return message.reply(reply);
   }
 };
